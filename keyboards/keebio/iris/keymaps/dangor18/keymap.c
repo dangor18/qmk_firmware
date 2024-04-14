9cba83086539eb9df7c5b0ccd99a8558e503bac5
Daniel <dangordon.za@gmail.com>
2024-04-14 12:31:36 +0200

    first version of incorporating tap and hold layer changes on a single key

--- keyboards/keebio/iris/keymaps/dangor18/keymap.c
+++ keyboards/keebio/iris/keymaps/dangor18/keymap.c
@@ -7,12 +7,14 @@
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 3
+#define TAPPING_TERM 200
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
   ADJUST,
+  MO_TG,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -21,13 +23,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     KC_NO,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB, MT(MOD_LSFT, KC_A), KC_S, MT(MOD_LCTL, KC_D), KC_F, KC_G,                 KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
+     KC_TAB,  KC_A,     KC_S,   KC_D,   KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+     KC_NO,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    KC_LGUI, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_RALT
+                                    KC_LGUI, KC_LSFT, KC_ENT,                    KC_SPC, MO_TG, KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -35,13 +37,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     QK_BOOT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_PGUP,  KC_UP,   KC_PGDN, KC_0,    _______,
+     QK_BOOT, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                KC_6,     KC_7,   KC_8,    KC_9,    KC_0,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_DEL,  KC_NO, KC_LEFT, KC_RGHT, KC_UP,   KC_LBRC,                               KC_RBRC, KC_LEFT, KC_DOWN,   KC_RGHT, KC_PLUS, KC_PIPE,
+     KC_NO,  LCTL(KC_A), LCTL(KC_S), KC_NO, LCTL(KC_F), KC_NO,                         KC_NO,   KC_UP,  KC_LEFT,  KC_RGHT, KC_NO, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     BL_STEP, _______, LCTL(KC_C), LCTL(KC_V), KC_DOWN, KC_LCBR, KC_LPRN,     LCTL(KC_V), KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, _______,
+     KC_NO, LCTL(KC_Z), KC_NO, LCTL(KC_C), LCTL(KC_V), KC_NO, KC_NO,          KC_NO, KC_RCBR, KC_DOWN,   KC_NO,   KC_NO,   KC_NO, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, KC_DEL,                    LCTL(KC_C),  _______, KC_P0
+                                    KC_NO, KC_LCTL, KC_NO,                  KC_NO, MO_TG, KC_NO
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -49,13 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_TOG, KC_EXLM, KC_AT,   KC_LEFT_BRACKET, KC_RIGHT_BRACKET,  KC_PERC,           KC_CIRC, KC_PLUS, KC_EQL, KC_LPRN, KC_RPRN, _______,
+     RGB_TOG, KC_NO, KC_NO, KC_LEFT_BRACKET, KC_RIGHT_BRACKET,  KC_PERC,           KC_CIRC, KC_PLUS, KC_EQL, KC_LPRN, KC_RPRN, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     RGB_MOD, KC_MPRV, KC_MNXT, KC_LCBR, KC_RCBR, KC_UNDS,                            KC_EQL,  KC_MINS, KC_UNDS, RGB_SAI, RGB_VAI, KC_BSLS,
+     RGB_MOD, KC_MPRV, KC_MNXT, KC_LCBR, KC_RCBR, KC_NO,                            KC_NO,  KC_MINS, KC_UNDS, RGB_SAI, RGB_VAI, KC_NO,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_MUTE, KC_MSTP, KC_MPLY, KC_BSLS, KC_PIPE, KC_MINS, KC_LPRN,          _______, KC_PLUS, KC_END,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
+     KC_MUTE, KC_MSTP, KC_MPLY, KC_BSLS, KC_PIPE, KC_MINS, KC_NO,          _______, KC_NO, KC_NO,  RGB_HUD, RGB_SAD, RGB_VAD, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _______,                   _______, _______, _______
+                                    _______, KC_NO, _______,                   _______, KC_NO, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -74,11 +76,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   )
 };
 
+void keyboard_post_init_user(void) {
+    // Set the default layer to _QWERTY each time the keyboard is powered on
+    set_single_persistent_default_layer(_QWERTY);
+    rgb_matrix_disable();
+    // Enable RGB lighting if needed
+    //rgb_matrix_enable();  // Or rgblight_enable() depending on your setup
+}
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint16_t mo_tg_timer;
   switch (keycode) {
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
+        rgb_matrix_disable();  // Turn off RGB lighting
       }
       return false;
       break;
@@ -110,6 +122,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+      case MO_TG:
+            if (record->event.pressed) {
+                // Key is pressed, start the timer
+                mo_tg_timer = timer_read();
+                layer_on(_RAISE);  // Temporarily activate _RAISE while held
+            } else {
+                // Key is released
+                layer_off(_RAISE);  // Deactivate _RAISE layer
+                if (timer_elapsed(mo_tg_timer) < TAPPING_TERM) {
+                    // It was a tap (not a hold)
+                    if (layer_state_is(_LOWER)) {
+                        // If currently on _LOWER, switch back to _QWERTY
+                        layer_move(_QWERTY);
+                        rgb_matrix_disable();  // Optionally turn off RGB lighting
+                    } else {
+                        // Otherwise, switch to _LOWER
+                        layer_move(_LOWER);
+                        rgb_matrix_set_color_all(0, 0, 255);  // Set all keys to blue
+                        rgb_matrix_enable();  // Optionally turn on RGB lighting
+                    }
+                }
+            }
+            return false;  // Skip further processing of this key
+            break;
   }
   return true;
 }
